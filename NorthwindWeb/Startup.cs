fff7e9aa428d30bf621c90e58e28544ebbeecfdf
TrumanQ <meng.t.qiu@gmail.com>
2020-04-03 07:40:05 -0500

    test confirm.

--- NorthwindWeb/Startup.cs
+++ NorthwindWeb/Startup.cs
@@ -17,7 +17,7 @@ namespace NorthwindWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            //System.Console.WriteLine();
+            //System.Console.WriteLine(); // just for confirmation
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
